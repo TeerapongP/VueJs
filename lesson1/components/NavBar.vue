6aa31a1e29ba72184d0a75d02b046a43ca110cf7
TeerapongP <std25406@snws.ac.th>
2022-09-10 12:49:02 +0700

    half-day

---
+++ lesson1/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="nav">
+    <nuxt-link to="/home" class="brand">
+      <img src="../static/images/meme/268904447_1084397152296790_641230717616496266_n.jpg">
+    </nuxt-link>
+    <nav>
+      <nuxt-link to="/">Home</nuxt-link>&nbsp|
+      <nuxt-link to="/about">About</nuxt-link>
+    </nav>
+  </div>
+</template>
+<style scoped>
+  img{
+    height: 5rem;
+
+    max-width: 100%;
+    border-radius: 50%;
+  }
+  .brand{
+    padding: 0.625rem;
+  }
+  .nav{
+    padding: 0.625rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 8rem;
+    font-size: 2rem;
+    background-color: lightgray;
+  }
+</style>
---
+++ lesson1/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img src="../static/images/meme/268796889_2448521615285393_144295133603000260_n.jpg">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  layout: 'default',
+}
+</script>
+<style>
+  img{
+    
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 100%;
+  }
+</style>
